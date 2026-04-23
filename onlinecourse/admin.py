29de711846b08032fbae3b5e0a99dc465769fbf2
Anushka Jarande <anushkajarande@gmail.com>
2026-04-23 12:54:17 +0530

    Updated admin.py for Task 2

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,25 +1,18 @@
 from django.contrib import admin
-from .models import Course, Lesson, Enrollment, Question, Choice, Submission, User
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission
 
-# Inline for Choice inside Question
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 2
 
-# Inline for Question inside Lesson
 class QuestionInline(admin.TabularInline):
     model = Question
-    extra = 1
 
-# Question Admin
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
-# Lesson Admin
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
 
-# Register models
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Enrollment)
