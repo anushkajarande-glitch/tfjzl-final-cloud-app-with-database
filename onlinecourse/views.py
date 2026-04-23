805f25b4d642f832771b1f2749619abc1155c27b
Anushka Jarande <anushkajarande@gmail.com>
2026-04-23 13:09:18 +0530

    Added views functions

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Question, Choice, Submission
 
-# Create your views here.
+def submit(request):
+    if request.method == 'POST':
+        selected_choices = request.POST.getlist('choices')
+        
+        submission = Submission.objects.create()
+        
+        for choice_id in selected_choices:
+            choice = Choice.objects.get(id=choice_id)
+            submission.choices.add(choice)
+        
+        return redirect('show_exam_result')
+    
+    return redirect('home')
+
+
+def show_exam_result(request):
+    submissions = Submission.objects.all().last()
+    selected_choices = submissions.choices.all()
+
+    score = 0
+    total = 0
+
+    for choice in selected_choices:
+        if choice.is_correct:
+            score += 1
+        total += 1
+
+    context = {
+        'score': score,
+        'total': total,
+    }
+
+    return render(request, 'result.html', context)
